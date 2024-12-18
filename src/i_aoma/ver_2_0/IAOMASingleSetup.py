99ddb67b5ba9e73ad3332661e80cdbe4f2b59977
marco_rosso_m <marco.rosso@polito.it>
2024-12-18 09:56:16 +0100

    failed using SingleSetupHandler in parallel due to segmentation fault

--- src/i_aoma/ver_2_0/IAOMASingleSetup.py
+++ src/i_aoma/ver_2_0/IAOMASingleSetup.py
@@ -6,30 +6,56 @@ Marco Martino Rosso
 """
 
 import numpy as np
-import matplotlib.pyplot as plt
+from joblib import Parallel, delayed
 
-
-from pyoma2.setup import SingleSetup
 # from pyoma2.functions.plot import plot_mac_matrix
+from .DataHandler import SingleSetupHandler
+from .QmcSampler import QmcSampler
 
-plt.plot([0, 0])
-plt.close()
+# plt.plot([0, 0])
+# plt.close()
 
 
 class IAOMASingleSetup:
-    def __init__(self, data: np.ndarray, fs: float):
-        self.ss = SingleSetup(data, fs=fs)
+    def __init__(
+        self,
+        data: np.ndarray,
+        fs: float,
+        NsimPh1: int,
+        ff: float = 0.5,
+        DecFct: int = 0,
+        detrend: bool = False,
+    ):
+        self.qmc = QmcSampler(data, fs, _ff=ff)
+        self.sampledpar = self.qmc.Halton(
+            _numsim=NsimPh1
+        )  # it samples an array of [time shift, max order, window lenght, time target]
 
-    def def_geo(self, *arg):
-        """
-        arg contains path to excel file for defining geometry according to PyOMA2 standard
-        _geo1,_geo2
-        """
-        try:
-            if len(arg) == 1:
-                self.ss.def_geo1_by_file(arg[0])
-            else:
-                self.ss.def_geo1_by_file(arg[0])
-                self.ss.def_geo2_by_file(arg[1])
-        except Exception:
-            raise ValueError("Invalid geometry type!")
+        # self.data_handler = SingleSetupHandler(data, fs, DecFct = DecFct, detrend = detrend)#, sampled_params=self.sampledpar[0])
+
+        # Parallelize the creation of SingleSetupHandler instances
+        self.data_handlers = Parallel(n_jobs=-1)(
+            delayed(SingleSetupHandler)(
+                data,
+                fs,
+                DecFct=DecFct,
+                detrend=detrend,
+                sampled_params=params,
+                NumAnal=NumAnal,
+            )
+            for NumAnal, params in enumerate(self.sampledpar)
+        )
+
+    # def def_geo(self, *arg):
+    #     """
+    #     arg contains path to excel file for defining geometry according to PyOMA2 standard
+    #     _geo1,_geo2
+    #     """
+    #     try:
+    #         if len(arg) == 1:
+    #             self.ss.def_geo1_by_file(arg[0])
+    #         else:
+    #             self.ss.def_geo1_by_file(arg[0])
+    #             self.ss.def_geo2_by_file(arg[1])
+    #     except Exception:
+    #         raise ValueError("Invalid geometry type!")
---
+++ src/i_aoma/ver_2_0/QmcSampler.py
@@ -0,0 +1,53 @@
+from scipy.stats import qmc
+import numpy as np
+
+
+class QmcSampler:
+    def __init__(self, data, fs, _ff=1):
+        # %% Define qmc sampling limits
+
+        self.fs = fs  # sampling frequency
+        self.ff = _ff  # fundamental mode frequency
+        # num of monitored dofs, i.e. tot num of sensors or, in general, channels
+        self.NDOFS = data.shape[1]
+        self.Ndata = data.shape[0]
+
+        # SSI time lag or time shift parameter (block rows)
+        self.TSmin = np.rint(fs / (2 * self.ff)).astype(int)
+        self.TSmax = 10 * self.TSmin
+
+        # order min and max
+        self.nmin = 2 * self.NDOFS
+        self.nmax = self.TSmax * self.NDOFS
+
+        # time window length
+        self.lmin = np.rint(2 / self.ff).astype(int)  # 3*TSmax #int(np.ceil(2/ff))
+        self.lmax = self.Ndata
+
+    def Halton(self, _dim=4, _scamble=True, _numsim=1):
+        HaltonSamples = qmc.Halton(d=_dim, scramble=_scamble).random(n=_numsim)
+        TS = self.TSmin + np.rint(
+            (self.TSmax - self.TSmin) * HaltonSamples[:, 0]
+        ).astype(int)
+        n = self.nmin + np.rint((self.nmax - self.nmin) * HaltonSamples[:, 1]).astype(
+            int
+        )
+        wlen = self.lmin + np.rint(
+            (self.lmax - self.lmin) * HaltonSamples[:, 2]
+        ).astype(int)
+        Tt = np.rint(HaltonSamples[:, 3] * self.Ndata).astype(int)
+
+        self.qmc_last_sampled_unitary = HaltonSamples
+        self.qmc_last_sampled = np.vstack([TS, n, wlen, Tt]).T
+        # Assuming self.qmc_last_sampled is a 2D array
+        self.qmc_last_sampled_list = [
+            list(sublist) for sublist in self.qmc_last_sampled
+        ]
+
+        return self.qmc_last_sampled_list
+
+    def retrieve_last_qmc_sampled(self, unitary=True):
+        if unitary:
+            return self.qmc_last_sampled_unitary
+        else:
+            return self.qmc_last_sampled
